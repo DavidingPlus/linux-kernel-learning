3d691d767b14fb62826fc0119ac0029a098df5ed
DavidingPlus <davidingplus@qq.com>
2024-10-29 03:46:26 +0000

    some changes

---
+++ src/calc.h
@@ -0,0 +1,10 @@
+#ifndef _CALC_H_
+#define _CALC_H_
+
+
+extern int add(int a, int b);
+
+extern int sub(int a, int b);
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-#include "add.h"
+#include "calc.h"
 
 
 MODULE_VERSION("1.0.0");
@@ -10,9 +10,9 @@ MODULE_AUTHOR("DavidingPlus");
 MODULE_DESCRIPTION("A Simple Hello World Module");
 
 
-static int helloInitData __initdata = -1;
+static int helloInitData __initdata = -114514;
 
-static const char *helloExitData __exitdata = "banana";
+static const char *helloExitData __exitdata = "abandon";
 
 
 static int __init hello_init(void)
@@ -25,7 +25,7 @@ static int __init hello_init(void)
 
 static void __exit hello_exit(void)
 {
-    printk(KERN_INFO "hello: Goodbye World %d %s\n", add(2, 3), helloExitData);
+    printk(KERN_INFO "hello: Goodbye World %d %s\n", sub(2, 3), helloExitData);
 }
 
 
