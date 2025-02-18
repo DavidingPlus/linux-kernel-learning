e0b2b8d62c072f33f12bfd54e903c82afafaf749
DavidingPlus <davidingplus@qq.com>
2025-02-18 02:00:11 +0000

    xmake v2.9.8 adaptation

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ option ("linux-headers", {showmenu = true, description = "Set linux-headers path
 
 
 target ("globalfifo")
-    add_rules ("platform.linux.driver")
+    add_rules ("platform.linux.module")
     add_files ("src/*.c")
     set_values ("linux.driver.linux-headers", "$(linux-headers)")
 
